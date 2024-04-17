af29f1d463a3872b0d1c51de2d9302da8c32b7f0
lenovo <madangowda1213@gmail.com>
2024-04-17 18:16:05 +0530

    Delete Modal Completed

--- src/main/resources/static/js/StudentList.js
+++ src/main/resources/static/js/StudentList.js
@@ -1,2 +1,32 @@
 'use strict';
 
+$(document).ready(function(){
+
+    $(document).on('click','#example .delete-student', function(){
+        let id = $(this).attr('id');
+        $.confirm({
+            title:'Delete Student',
+            content:'Are you sure you want to delete student?',
+            type:'red',
+            icon:'fa-regular fa-face-frown fa-lg',
+            buttons:{
+                submit:{
+                    text:'Delete',
+                    btnClass:'btn btn-danger',
+                    action:function(){
+                        fetch('/delete/'+id,{
+                            method:'get'
+                        }).then(response=>response.json())
+                        .then(data=> console.log(data));
+                    }
+                },
+                close:{
+                    text:'Close',
+                    btnClass:'btn btn-secondary',
+                    action:function(){}
+                }
+            }
+        });
+    });
+
+});
